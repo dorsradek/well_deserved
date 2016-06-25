888c7ca65126d358295e0c669d61a8b3eb7db0ba
Marta Bondyra <marta@Martas-MacBook-Pro.local>
2016-06-25 10:25:44 +0200

    init front

---
+++ frontEndParent/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontEndParentApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the frontEndParentApp
+ */
+angular.module('frontEndParentApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ frontEndParent/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontEndParentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontEndParentApp
+ */
+angular.module('frontEndParentApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
